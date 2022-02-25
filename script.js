03e634231155faae9b863f82029e99d81a484377
LaMarr Smith <ljsmith19@my.trine.edu>
2022-02-25 09:48:14 -0500

    Added digit buttons, operator buttons, and switch statement

--- script.js
+++ script.js
@@ -12,7 +12,22 @@ const divide = (a, b) => {
 }
 
 const operator = (operand, a, b) => {
-    if (operand == '+'){
-        add(a,b);
-    }
+    const operation = operand;
+    switch(operation){
+        case '+':
+            add(a, b);
+            return add(a,b);
+        case '-':
+            subtract(a,b);
+            return subtract(a,b);
+        case '*':
+            multiply(a,b);
+            return multiply(a,b);
+        case '/':
+            divide(a,b);
+            return divide(a,b);
+        default:
+            alert("Invalid operator entered!");
+    };
+ 
 }
