9a9d1bee9b7f98a7fa8a8c89e528a67d30d5e6ad
LaMarr Smith <ljsmith19@my.trine.edu>
2022-02-25 08:45:30 -0500

    Started creating the operations

---
+++ script.js
@@ -0,0 +1,18 @@
+const add = (a,b) => {
+    return a + b;
+};
+const subtract = (a, b) => {
+    return a - b;
+};
+const multiply = (a, b) => {
+    return a * b;
+}
+const divide = (a, b) => {
+    return a / b;
+}
+
+const operator = (operand, a, b) => {
+    if (operand == '+'){
+        add(a,b);
+    }
+}
