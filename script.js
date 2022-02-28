adde66c1d47323163e679aa2b7e4d91f543f1bfa
LaMarr Smith <ljsmith19@my.trine.edu>
2022-02-28 09:48:41 -0500

    Added switch statement for my operator buttons need to figure out how to get actual calculator part working

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 const btnContainer = document.querySelector('.buttonNums');
 const btnNumSelector = btnContainer.getElementsByClassName('btn');
+const btnOps = document.querySelector('.buttonOps');
+const btnOpsSelector = btnOps.getElementsByClassName('btn');
 const displayText = document.getElementById('displayText');
-// Selects all of the buttons and adds event listeners to them
+let holdNum = 0;
+// Selects all of the number buttons and adds event listeners to them
 for (let i = 0; i < btnNumSelector.length; i++){
     btnNumSelector.item(i).addEventListener('click', () =>{
         let displayValue = i;
         displayText.innerHTML = displayValue;
+        return holdNum;
     })
 }
 
+// Selects all of the operators and adds event listeners to them
+for (let o = 0; o < btnOpsSelector.length; o++){
+    btnOpsSelector.item(o).addEventListener('click', ()=>{
+        let displayValue = o;
+        switch(displayValue){
+            case 0:
+                displayValue = '+';
+                operate(displayValue, holdNum, holdNum)
+                displayText.innerHTML = displayValue;
+                break;
+            case 1:
+                displayValue = '-';
+                displayText.innerHTML = displayValue;
+                break;
+            case 2:
+                displayValue = '*';
+                displayText.innerHTML = displayValue;
+                break;
+            case 3: 
+                displayValue = '/';
+                displayText.innerHTML = displayValue;
+                break;
+            case 4:
+                displayValue = '='
+                displayText.innerHTML = sum;
+                break;
+        }
+    })
+}
 const add = (a,b) => {
     return a + b;
 };
