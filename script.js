1dc536c3d5b57176ef2b8b4138c30f496665bf17
LaMarr Smith <ljsmith19@my.trine.edu>
2022-03-15 15:10:31 -0400

    Have been debugging the calculator for a week now Trying to get the calculator to do the second half of the calculation

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const btnNumSelector = btnContainer.getElementsByClassName('btn');
 const btnOps = document.querySelector('.buttonOps');
 const btnOpsSelector = btnOps.getElementsByClassName('btn');
 const displayText = document.getElementById('displayText');
-
-let blank = "";
+let blank = [];
 let displayV = "";
+let runTT = 0; // Running total to keep track of sum
+
 // Selects all of the number buttons and adds event listeners to them
-for (let i = 0; i < btnNumSelector.length; i++){
-    btnNumSelector.item(i).addEventListener('click', () =>{
-        blank += i; // Adds the number clicked to the blank variable
-        displayV += i; // Adds the number to the display value variable
-        displayText.innerHTML = displayV;
-        return blank;
-    })
+function btnSelector (){
+    for (let i = 0; i < btnNumSelector.length; i++){
+        btnNumSelector.item(i).addEventListener('click', () =>{
+            blank += i; // Adds the number clicked to the blank variable
+            displayV += i; // Adds the number to the display value variable
+            displayText.innerHTML = displayV;
+            return blank;
+        })
+    }
 }
 
 // Selects all of the operators and adds event listeners to them
@@ -24,9 +27,11 @@ for (let o = 0; o < btnOpsSelector.length; o++){
             case 0:
                 displayValue = '+';
                 blank += displayValue; // Adds the operator to the blank variable
+                
                 displayV = " "; 
                 displayText.innerHTML = " ";
                 displayText.innerHTML = displayValue;
+                // let runT = test(blank);
                 break;
             case 1:
                 displayValue = '-';
@@ -48,26 +53,81 @@ for (let o = 0; o < btnOpsSelector.length; o++){
                 break;
             case 4:
                 displayValue = '='
-                test(blank);
+                let jj = test(blank);
+                runTT = jj;
+                displayText.innerHTML = jj;
                 break;
             case 5:
                 displayText.innerHTML = 0;
                 displayV = "";
                 blank = "";
+                runTT = 0;
         }
     })
 }
 
 function test(){
-    for (let i = 0; i < blank.length; i++){
-        let a = parseInt(blank[i]);
-        if (a != parseInt){
-            console.log(blank[i])
+    let runT = 0;
+    for (let i = 0; i < blank.length; i++){ // Starts the for loop that goes through the length of the string
+        if (runT == 0 && blank[i] == '+' || blank[i] == '-' || blank[i]== '*' || blank[i] == '/' ){ // Starting if statement to get the running total its first non-zero value
+            let arSlice = parseInt(blank.slice(0, i)); // Makes the left-side of the operator into an int
+            let x = blank[i]; // The index of the operator
+            let arEnd; // Sets the right-side variable
+            for (let j = i+1; j <= blank.length; j++){ // Moves the index to the first right-side value and starts the for loop to get the entire length of the value
+                if(blank[j] == '+' || blank[j] == '-' || blank[j]== '*' || blank[j] == '/' ){
+                    arEnd = parseInt(blank.slice(i + 1, j)); // Makes the right-side of the operator into an int
+                    let value = operate(x, arSlice, arEnd);
+                    runT = value;
+                    break; // Stops the first operation code if there is more than one operator
+                }
+                if(j == blank.length){ // If the index is equal to the length of the string, return the sum
+                    let arEnd = parseInt(blank.slice(i + 1));
+                    let value = operate(x, arSlice, arEnd);
+                    runT = value;
+                    return value;
+                }
+            }
+            
         }
+        
+        if (runT !=0){
+            
+            let arSlice = runT;
+            
+            let arEnd;
+            
+            for (let j = i+1; j <= blank.length; j++){ // Moves the index to the first right-side value and starts the for loop to get the entire length of the value
+                if(blank[j] == '+' || blank[j] == '-' || blank[j]== '*' || blank[j] == '/' ){
+                    arEnd = parseInt(blank.slice(blank[j] + 1, blank.length)); // Makes the right-side of the operator into an int
+                    
+                    let value = operate(blank[j], arSlice, arEnd);
+                    runT = value;
+                }
+                if(j == blank.length){
+                    arEnd = parseInt(blank.slice(blank[j] + 1, blank.length));
+                    value = operate(blank[j], arSlice, arEnd);
+                    console.log(blank[j], arSlice, arEnd);
+                    runT = value;
+                    return value;
+                }
+            }
+
+            //     }
+            //     if(j == blank.length){
+            //         let arEnd = parseInt(blank.slice(i + 1));
+            //         let value = operate(j, arSlice, arEnd);
+            //         return value;
+            //     }
+            //     let value = operate(x, arSlice, arEnd);
+            //     runT += value;
+            // }
+            
+            
+        }
+
     }
 }
-
-
+   
 const add = (a,b) => {
     return a + b;
 };
@@ -78,6 +138,9 @@ const multiply = (a, b) => {
     return a * b;
 }
 const divide = (a, b) => {
+    if (b == 0){
+        displayText.innerHTML = 'Cannot divide by zero! Clear and try again!';
+    }
     return a / b;
 }
 
@@ -102,3 +165,4 @@ const operate = (operand, a, b) => {
  
 }
 
+btnSelector();
