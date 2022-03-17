5dc7b994ba38b5b157079022a77ed844828245e3
LaMarr Smith <ljsmith19@my.trine.edu>
2022-03-17 14:24:29 -0400

    Looks somewhat stylish now Changing the JS functions

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const btnContainer = document.querySelectorAll('.btn');
 // const btnNumSelector = btnContainer.getElementsByClassName('btn');
-const btnOps = document.querySelector('.buttonOps');
-const btnOpsSelector = btnOps.getElementsByClassName('btn');
 const currentDisplay = document.getElementById('currentDisplay');
 const previousDisplay = document.getElementById('previousDisplay');
 let blank = [];
@@ -19,65 +17,65 @@ let runT = 0;
 //         })
 //     }
 // }
-console.log(btnContainer)
 btnContainer.forEach(button =>{
     button.addEventListener('click', ()=>{
-        console.log(button)
-        blank += i; // Adds the number clicked to the blank variable
-        displayV += i; // Adds the number to the display value variable
+        blank.push(button.innerHTML) // Adds the number clicked to the blank variable
+        displayV += button.innerHTML; // Adds the number to the display value variable
+        console.log(blank)
+        currentDisplay.innerText = displayV;
     })
 })
 // Selects all of the operators and adds event listeners to them
-for (let o = 0; o < btnOpsSelector.length; o++){
-    btnOpsSelector.item(o).addEventListener('click', ()=>{
-        let displayValue = o;
-        switch(displayValue){
-            case 0:
-                displayValue = '+';
-                blank += displayValue; // Adds the operator to the blank variable
-                displayV = " "; 
-                currentDisplay.innerHTML = " ";
-                currentDisplay.innerHTML = displayValue;
-                break;
-            case 1:
-                displayValue = '-';
-                blank += displayValue;
-                displayV = " "; 
-                currentDisplay.innerHTML = displayValue;
-                break;
-            case 2:
-                displayValue = '*';
-                blank += displayValue;
-                displayV = " "; 
-                currentDisplay.innerHTML = displayValue;
-                break;
-            case 3: 
-                displayValue = '/';
-                blank += displayValue;
-                displayV = " "; 
-                currentDisplay.innerHTML = displayValue;
-                break;
-            case 4:
-                if (blank.length == 0){
-                    return;
-                }
-                displayValue = '='
-                let result = test(blank);
-                runT = result;
-                previousDisplay.innerHTML = blank + "=";
-                blank = [];
-                blank += runT;
-                currentDisplay.innerHTML = result;
-                break;
-            case 5:
-                clear();
-            case 6:
-                del();
-            default:
-                return;
-        }
-    })
-}
+// for (let o = 0; o < btnOpsSelector.length; o++){
+//     btnOpsSelector.item(o).addEventListener('click', ()=>{
+//         let displayValue = o;
+//         switch(displayValue){
+//             case 0:
+//                 displayValue = '+';
+//                 blank += displayValue; // Adds the operator to the blank variable
+//                 displayV = " "; 
+//                 currentDisplay.innerHTML = " ";
+//                 currentDisplay.innerHTML = displayValue;
+//                 break;
+//             case 1:
+//                 displayValue = '-';
+//                 blank += displayValue;
+//                 displayV = " "; 
+//                 currentDisplay.innerHTML = displayValue;
+//                 break;
+//             case 2:
+//                 displayValue = '*';
+//                 blank += displayValue;
+//                 displayV = " "; 
+//                 currentDisplay.innerHTML = displayValue;
+//                 break;
+//             case 3: 
+//                 displayValue = '/';
+//                 blank += displayValue;
+//                 displayV = " "; 
+//                 currentDisplay.innerHTML = displayValue;
+//                 break;
+//             case 4:
+//                 if (blank.length == 0){
+//                     return;
+//                 }
+//                 displayValue = '='
+//                 let result = test(blank);
+//                 runT = result;
+//                 previousDisplay.innerHTML = blank + "=";
+//                 blank = [];
+//                 blank += runT;
+//                 currentDisplay.innerHTML = result;
+//                 break;
+//             case 5:
+//                 clear();
+//             case 6:
+//                 del();
+//             default:
+//                 return;
+//         }
+//     })
+// }
 
 function test(){
     for (let i = 0; i < blank.length; i++){ // Starts the for loop that goes through the length of the string
